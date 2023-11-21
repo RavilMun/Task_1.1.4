abfb65d7fce647cad76145dac92fabfda3b43a2b
RavilMun <146236487+RavilMun@users.noreply.github.com>
2023-11-21 21:42:16 +0500

    Update UserServiceImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,7 +7,7 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private final static UserDao USER_DAO = UserDaoJDBCImpl.getInstance();
+    private final UserDao USER_DAO = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
         USER_DAO.createUsersTable();
